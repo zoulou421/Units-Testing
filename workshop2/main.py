21d1afc6b9d067ec1fbe76253590a886bd13a546
Bonevy BEBY <bonevybeby@gmail.com>
2024-04-29 08:56:47 +0200

    Test expecting SUCCESS(positive test)//TDD

--- workshop2/main.py
+++ workshop2/main.py
@@ -1,2 +1,4 @@
 def add(a, b):
+    if isinstance(a,str) or isinstance(b,str):
+        return None #this will fail the test:test failure(test_add_two_letters method)
     return a + b
--- workshop2/test.py
+++ workshop2/test.py
@@ -8,6 +8,16 @@ class TestCalculator(TestCase):
     def test_add_two_positive_numbers(self):
         self.assertEqual(add(1, 2), 3)
 
+    def test_add_two_letters(self):
+        self.assertEqual(add("a", "b"), "ab")  #will not work because
+        # of the condition isinstance in current main.py file
+
+    def test_add_two_booleans(self):
+        self.assertEqual(add(True, False), 1)
+        self.assertEqual(add(True, True), 2)
+        self.assertEqual(add(False, True), 1)
+        self.assertEqual(add(False, False), 0)
+
 ##For specific test using main module
 # main()
 # make a test in terminal:  python workshop2/test.py |  python workshop2/test.py -v(--verbose)
